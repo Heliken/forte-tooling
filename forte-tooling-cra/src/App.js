c0a3a68b1557afc09d09de2b7362872e918a91c3
Heliken <burshf@gmail.com>
2020-06-16 13:13:08 +0300

    SCSS modules with styled components

--- forte-tooling-cra/src/App.js
+++ forte-tooling-cra/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Button from "./Button.js";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button>Button</Button>
       </header>
     </div>
   );
